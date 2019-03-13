561be4155fbf283b827af47d8d4c57f7294c86cc
Bob Rockers <rockers@okstate.edu>
2019-03-13 18:26:58 -0500

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "info.dat" << endl;
+	cout << "Number of A's: 0" << endl;
+	cout << "Number of F's: 0" << endl;
+	cout << "Number of a's: 1" << endl;
+	cout << "Number of b's: 0" << endl;
+	
+	return 0;
+}
